ed0025f308b6e28f5e4716b5f89b17ee6df98cca
kahoragachau <joelkahoragachau6@gmail.com>
2022-03-23 10:54:25 +0300

    -Add few Objects to the filter constructor for testing purposes

--- pages/rent/js/scripts.js
+++ pages/rent/js/scripts.js
@@ -5,3 +5,8 @@ function Filter(location, noOfRoom, price){
     this.noOfRoom = noOfRoom;
     this.price = price;
 }
+
+// Add Objects to Filter Constructor
+let firstHouse = new Filter("Roysambu", 3, 50000);
+let secondHouse = new Filter("Karen", 4, 80000);
+let thirdHouse = new Filter("Roysambu", 2, 20000);
