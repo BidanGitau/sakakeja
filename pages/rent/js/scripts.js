6270780838b06cb7c0cbed0ee29793c8a0de0121
kahoragachau <joelkahoragachau6@gmail.com>
2022-03-23 21:39:31 +0300

    - Delete Unnecessary Code

--- pages/rent/js/scripts.js
+++ pages/rent/js/scripts.js
@@ -1,41 +1,20 @@
-// Business Logic
-// Make constructor function for the filter function
-function Filter(location, noOfRoom, price){
-    this.location = location;
-    this.noOfRoom = noOfRoom;
-    this.price = price;
-}
-
-// Add Objects to Filter Constructor
-let firstHouse = new Filter("Roysambu", 3, 50000);
-let secondHouse = new Filter("Karen", 4, 80000);
-let thirdHouse = new Filter("Roysambu", 2, 20000);
-
-// User Logic
 $(document).ready(function(){
     $("#filter-button").click(function(event){
-        $(".card").hide();
-        let filterByRooms = $("#rooms").val();
         let filterByLocation = $("#location").val();
-        let filterByPrice = $("#price").val();
-        let filterOptions = [firstHouse, secondHouse, thirdHouse];
-        let inputOptions = [filterByRooms, filterByLocation, filterByPrice];
-
-        for (let i = 0; i < inputOptions.length; i++){
-            if( inputOptions[i] === ""){
-                alert("Inputs Can't Be Empty");
-            }
+        if(filterByLocation === "Karen"){
+            $(".karen").show();
+            $(".roysambu").hide();
+            $(".runda").hide();
         }
-
-        if (filterByRooms !=="" && filterByLocation !== "" && filterByPrice !==""){
-            for(let i = 0; i<filterOptions.length; i++){
-                if ((filterOptions[i].location === filterByLocation)){
-                    $(".card").filter(".karen").show();
-                }
-                else{
-                    $(".card").filter(".roysambu").hide();
-                }
-            }
+        if (filterByLocation === "Roysambu"){
+            $(".karen").hide();
+            $(".runda").hide()
+            $(".roysambu").show();
+        }
+        if(filterByLocation === "Runda"){
+            $(".runda").show();
+            $(".karen").hide();
+            $(".roysambu").hide();   
         }
     });
 })
