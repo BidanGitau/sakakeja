fdf0ce328d4856875d36688ee56c1c9981cf8d26
kahoragachau <joelkahoragachau6@gmail.com>
2022-03-23 10:48:20 +0300

    - Make A constructor for the filter

---
+++ pages/rent/js/scripts.js
@@ -0,0 +1,7 @@
+// Business Logic
+// Make constructor function for the filter function
+function Filter(location, noOfRoom, price){
+    this.location = location;
+    this.noOfRoom = noOfRoom;
+    this.price = price;
+}
