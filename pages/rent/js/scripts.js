3a110401caff6a28f195f428d4f91008c1e75706
kahoragachau <joelkahoragachau6@gmail.com>
2022-03-23 15:58:15 +0300

    -Add an easy way to solve the filter problem

--- pages/rent/js/scripts.js
+++ pages/rent/js/scripts.js
@@ -10,3 +10,32 @@ function Filter(location, noOfRoom, price){
 let firstHouse = new Filter("Roysambu", 3, 50000);
 let secondHouse = new Filter("Karen", 4, 80000);
 let thirdHouse = new Filter("Roysambu", 2, 20000);
+
+// User Logic
+$(document).ready(function(){
+    $("#filter-button").click(function(event){
+        $(".card").hide();
+        let filterByRooms = $("#rooms").val();
+        let filterByLocation = $("#location").val();
+        let filterByPrice = $("#price").val();
+        let filterOptions = [firstHouse, secondHouse, thirdHouse];
+        let inputOptions = [filterByRooms, filterByLocation, filterByPrice];
+
+        for (let i = 0; i < inputOptions.length; i++){
+            if( inputOptions[i] === ""){
+                alert("Inputs Can't Be Empty");
+            }
+        }
+
+        if (filterByRooms !=="" && filterByLocation !== "" && filterByPrice !==""){
+            for(let i = 0; i<filterOptions.length; i++){
+                if ((filterOptions[i].location === filterByLocation)){
+                    $(".card").filter(".karen").show();
+                }
+                else{
+                    $(".card").filter(".roysambu").hide();
+                }
+            }
+        }
+    });
+})
