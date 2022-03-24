efca894c55879d00433b1d3003d644b1461649b0
kahoragachau <joelkahoragachau6@gmail.com>
2022-03-24 11:56:01 +0300

    - Remove Case Sensitivity from search input

--- pages/rent/js/scripts.js
+++ pages/rent/js/scripts.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){
     $("#filter-button").click(function(event){
         let filterByLocation = $("#location").val();
-        if(filterByLocation === "Karen"){
+        if(filterByLocation.toLowerCase() === "karen"){
             $(".karen").show();
             $(".roysambu").hide();
             $(".runda").hide();
         }
-        if (filterByLocation === "Roysambu"){
+        if (filterByLocation.toLowerCase() === "roysambu"){
             $(".karen").hide();
             $(".runda").hide()
             $(".roysambu").show();
         }
-        if(filterByLocation === "Runda"){
+        if(filterByLocation.toLowerCase() === "runda"){
             $(".runda").show();
             $(".karen").hide();
             $(".roysambu").hide();   
