8486e49c49b8f65e56fac59a4a5c481abdd16a3b
kahoragachau <joelkahoragachau6@gmail.com>
2022-03-24 12:10:24 +0300

    Added Some configurations

--- pages/rent/js/scripts.js
+++ pages/rent/js/scripts.js
@@ -2,16 +2,19 @@ $(document).ready(function(){
     $("#filter-button").click(function(event){
         let filterByLocation = $("#location").val();
         if(filterByLocation.toLowerCase() === "karen"){
+            $(".display-result").text("Showing Results For Houses In " + filterByLocation);
             $(".karen").show();
             $(".roysambu").hide();
             $(".runda").hide();
         }
         if (filterByLocation.toLowerCase() === "roysambu"){
+            $(".display-result").text("Showing Results For Houses In " + filterByLocation);
             $(".karen").hide();
             $(".runda").hide()
             $(".roysambu").show();
         }
         if(filterByLocation.toLowerCase() === "runda"){
+            $(".display-result").text("Showing Results For Houses In " + filterByLocation);
             $(".runda").show();
             $(".karen").hide();
             $(".roysambu").hide();   
