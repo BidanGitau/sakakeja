fc0fa928b5928bfc5bc2d758bb9d5a6640e9696f
kahoragachau <joelkahoragachau6@gmail.com>
2022-03-24 21:20:31 +0300

    -Add some colors

--- pages/rent/js/scripts.js
+++ pages/rent/js/scripts.js
@@ -4,31 +4,20 @@ $(document).ready(function(){
         if(filterByLocation.toLowerCase() === "karen"){
             $(".display-result").text("Showing Results For Houses In " + filterByLocation);
             $(".karen").fadeIn(3000);
-            $(".button1").fadeIn(2500);
             $(".roysambu").fadeOut();
             $(".runda").fadeOut();
         }
         if (filterByLocation.toLowerCase() === "roysambu"){
             $(".display-result").text("Showing Results For Houses In " + filterByLocation);
+            $(".roysambu").fadeIn(3000);
             $(".karen").fadeOut();
             $(".runda").fadeOut()
-            $(".roysambu").fadeIn(3000);
-            $(".button2").fadeIn(2500);
         }
         if(filterByLocation.toLowerCase() === "runda"){
             $(".display-result").text("Showing Results For Houses In " + filterByLocation);
             $(".runda").fadeIn(3000);
-            $(".button3").fadeIn(2500);
             $(".karen").fadeOut();
             $(".roysambu").fadeOut();   
         }
-    }),
-    $(".button1").click(function(event){
-        $(".display-result").text("Showing Results For Houses In Karen");
-        $(".karen").fadeIn(3000);
-        $(".button2").hide();
-        $(".button3").hide();
-        $(".roysambu").fadeOut();
-        $(".runda").fadeOut();
     })
 })
